12d6724c74e2b0fe4e422b588e9be7cc73d4b3cb
Mercè Bou Cintas <Merce@MacBook-Pro-de-Merce.local>
2016-10-19 10:50:18 +0200

    First submit

---
+++ js/functions_map2.js
@@ -0,0 +1,34 @@
+ // JavaScript Document "map_presentation"
+// Modal when load page
+
+$('#myModal2').modal('show');
+
+// MODAL prev and next
+
+$("div[id^='myModal']").each(function(){
+  
+  var currentModal = $(this);
+  
+  //click next
+  currentModal.find('.btn-next').click(function(){
+    currentModal.modal('hide');
+    currentModal.closest("div[id^='myModal']").nextAll("div[id^='myModal']").first().modal('show'); 
+  });
+  
+  //click prev
+  currentModal.find('.btn-prev').click(function(){
+    currentModal.modal('hide');
+    currentModal.closest("div[id^='myModal']").prevAll("div[id^='myModal']").first().modal('show'); 
+  });
+
+});
+
+// SLIDER
+$('#ex1').slider({
+  formatter: function(value) {
+    return 'Current value: ' + value;
+  }
+});
+$("#ex1").on("slide", function(slideEvt) {
+	$("#ex6SliderVal").text(slideEvt.value);
+});
