91ef843aaefe3130d6f08dab8caefa1af369bddc
German <german.mendoza@uji.es>
2017-06-09 10:59:37 +0200

    leaflet language examples

--- js/functions_globalEnd.js
+++ js/functions_globalEnd.js
@@ -15,7 +15,7 @@ function startAll() {
 
         var testEmail = /^[A-Z0-9._%+-]+@([A-Z0-9-]+\.)+[A-Z]{2,4}$/i;
         var mail = $("#mail_user").val();
-        var testtwitter = /^@?(\w){1,15}$/;
+        var testtwitter = /^@(\w){1,15}$/;
         var twitter = $("#twitter_name").val();
         var id = util.getFromLocalStorage(util.interPageDataKey);
 
